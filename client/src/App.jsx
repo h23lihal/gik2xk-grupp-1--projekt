4ac8613468a4e65418e28dfb66c09c451a85a6a3
h23lihal <l.hellstrom95@gmail.com>
2025-03-25 14:55:35 +0100

    Hemskämen stylad och klar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
-// src/App.jsx
 import React, { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { Box, AppBar, Toolbar, Typography, Button } from '@mui/material';
 import CartModal from './components/CartModal';
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import HomeIcon from '@mui/icons-material/Home';
 
 function App() {
@@ -18,30 +17,41 @@ function App() {
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" sx={{ backgroundColor: 'gold' }}>
           <Toolbar>
-            
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <Link to="/" style={{textDecoration: "none",color: "black",  display: "flex", alignItems: "center", gap: "5px" }}>
-         <HomeIcon sx={{ color: "black" }} />
-         Hem
-        </Link>
-        </Typography>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link
+                to="/"
+                style={{
+                  textDecoration: 'none',
+                  color: '#003366',
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: '5px',
+                }}
+              >
+                <HomeIcon sx={{ color: '#003366' }} />
+                Hem
+              </Link>
+            </Typography>
 
             {/* Lägg till produkt-knapp */}
             <Button color="inherit">
-              <Link to="/products/new" style={{ textDecoration: 'none', color: 'black' }}>
+              <Link to="/products/new" style={{ textDecoration: 'none', color: '#003366' }}>
                 Lägg till produkt
               </Link>
             </Button>
-        
-            <Button color="inherit" onClick={openModal} startIcon={<ShoppingCartIcon sx={{ color: "black"}}/>} 
-            sx={{ color: 'black' }}>
+
+            <Button
+              color="inherit"
+              onClick={openModal}
+              startIcon={<ShoppingCartIcon sx={{ color: '#003366' }} />}
+              sx={{ color: '#003366' }}
+            >
               varukorg
             </Button>
           </Toolbar>
         </AppBar>
       </Box>
 
-    
       <Outlet />
 
       {/* Footer */}
@@ -50,14 +60,42 @@ function App() {
         sx={{
           width: '100%',
           bgcolor: 'gold',
-          color: 'black',
+          color: '#003366',
           textAlign: 'center',
-          py: 2,
+          py: 4,
           mt: 4,
         }}
       >
-        <Typography variant="body2">
+        {/* Copyright och info */}
+        <Typography variant="body2" sx={{ mb: 2 }}>
+          &copy; {new Date().getFullYear()} TeaTime AB. Alla rättigheter reserverade.
         </Typography>
+
+        {/* Navigationslänkar */}
+        <Box sx={{ display: 'flex', justifyContent: 'center', gap: 3, mb: 2 }}>
+          <Link to="/privacy-policy" style={{ textDecoration: 'none', color: '#003366' }}>
+            Sekretesspolicy
+          </Link>
+          <Link to="/terms" style={{ textDecoration: 'none', color: '#003366' }}>
+            Användarvillkor
+          </Link>
+          <Link to="/contact" style={{ textDecoration: 'none', color: '#003366' }}>
+            Kontakt
+          </Link>
+        </Box>
+
+        {/* Sociala medier ikoner */}
+        <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2 }}>
+          <Link to="#" style={{ textDecoration: 'none', color: 'black' }}>
+            <i className="fab fa-facebook" style={{ fontSize: '24px' }}></i> {/* Facebook-ikon */}
+          </Link>
+          <Link to="#" style={{ textDecoration: 'none', color: 'black' }}>
+            <i className="fab fa-twitter" style={{ fontSize: '24px' }}></i> {/* Twitter-ikon */}
+          </Link>
+          <Link to="#" style={{ textDecoration: 'none', color: 'black' }}>
+            <i className="fab fa-instagram" style={{ fontSize: '24px' }}></i> {/* Instagram-ikon */}
+          </Link>
+        </Box>
       </Box>
 
       <CartModal customerId={customerId} isOpen={isModalOpen} onClose={closeModal} />
